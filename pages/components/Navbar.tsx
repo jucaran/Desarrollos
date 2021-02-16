2d21baf39a16c583a6386e21212ec779bcd9c259
Juanque <juan.castro.arancibia@gmail.com>
2021-02-16 18:13:59 -0300

    fixes nav styling

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -2,63 +2,46 @@ import * as React from "react";
 import styled from "styled-components";
 import Image from "next/image";
 
-interface NavbarProps {}
-
 const StyledNavbar = styled.nav`
   width: 100vw;
-  padding: 0 1rem;
+  padding: 1.2rem 2rem;
   background: rgba(39, 39, 39, 0.6);
   position: fixed;
   top: 0;
   left: 0;
 
-  ul {
-    padding: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    #chat_icon {
-      margin: 2rem;
-    }
+  .nav_icon:last-child {
+    margin-left: 0.5rem;
+    /* background: pink; */
+  }
 
-    img {
-      cursor: pointer;
-    }
+  img {
+    cursor: pointer;
   }
 `;
+
+interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = () => {
   return (
     <StyledNavbar>
-      <ul>
-        <li>
-          <Image
-            id="menu_icon"
-            src="/menu_icon.svg"
-            width={35}
-            height={30}
-            alt="Menú"
-          />
-        </li>
-        <li>
-          <Image
-            id="chat_icon"
-            className="chat_icon"
-            src="/chat_icon.svg"
-            width={35}
-            height={35}
-            alt="Contacto"
-          />
-          <Image
-            id="cart_icon"
-            src="/cart_icon.svg"
-            width={35}
-            height={35}
-            alt="Carrito"
-          />
-        </li>
-      </ul>
+      <div className="left">
+        <span className="menu_icon">
+          <Image src="/menu_icon.svg" width={35} height={30} alt="Menú" />
+        </span>
+      </div>
+      <div className="right">
+        <span className="nav_icon">
+          <Image src="/chat_icon.svg" width={35} height={35} alt="Contacto" />
+        </span>
+        <span className="nav_icon">
+          <Image src="/cart_icon.svg" width={35} height={35} alt="Carrito" />
+        </span>
+      </div>
     </StyledNavbar>
   );
 };
